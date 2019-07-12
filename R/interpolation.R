44277065da9b97a64e0060a7b514c303570437e5
jetstreamokayasu <tokotsuku2@outlook.jp>
2019-07-12 09:20:43 +0900

    Error occured when there isn't a point to add in function "origin_coordinate." Revised it

--- R/interpolation.R
+++ R/interpolation.R
@@ -89,7 +89,9 @@ voronoi_border<-function(vics, figure){
 
   exist<-exist_convexhull_check(vics_pca, insecs)
 
-  vics_oricord<-origin_coordinate(vics_pca, insecs[which(exist==T), ], figure[vics[1],])
+  if(length(insecs[which(exist==T), ]) > 0){
+    vics_oricord<-origin_coordinate(vics_pca, insecs[which(exist==T), ], figure[vics[1],])
+  }
 
   return(list(oricord=vics_oricord, pca_inter=insecs[which(exist==T), ]))
 
